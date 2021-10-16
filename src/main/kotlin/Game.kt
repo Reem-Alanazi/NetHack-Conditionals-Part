7826e2ec3ce6595dfa5b8e5d1fb138bec7fd54cc
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-16 12:31:50 +0300

    Improving aura code with conditional expression

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -5,26 +5,25 @@ fun main() {
     val isBlessed : Boolean = true
     val isImmortal = false
 
+    //Aura
+    //Improving aura code with a conditional expression
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    if (auraVisible){
-        println("GREEN")
-    } else{
-        println("NONE")
-    }
-//  Using a conditional expression
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+     println(auraColor)
+
     val healthStatus: String = if (healthPoints == 100){
-        (" is in excellent condition!")
+        ("is in excellent condition!")
     } else if (healthPoints >= 90) {
-        (" has a few scratches.")
+        ("has a few scratches.")
     } else if (healthPoints >= 75) {
         if (isBlessed) {
-            (" has some minor wounds but is healing quite quickly!")
+            ("has some minor wounds but is healing quite quickly!")
         } else {
-            (" has some minor wounds.") }
+            ("has some minor wounds.") }
     } else if (healthPoints >= 15) {
-        (" looks pretty hurt.")
+        ("looks pretty hurt.")
     } else {
-        (" is in awful condition!")
+        ("is in awful condition!")
     }
     //Player status
     println(name + " " + healthStatus)
