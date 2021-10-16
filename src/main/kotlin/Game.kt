9828759d62216e28311698b5f7b0f1b769ac8bde
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-16 11:02:15 +0300

    track if player is Blessed

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,17 +2,20 @@ fun main() {
 
     val name = "Madrigal"
     var healthPoints = 89
+    var isBlessed : Boolean = true  // new variable
 
     if (healthPoints == 100){
         println(name + " is in excellent condition!")
-        //  Checking for more player conditions
      } else if (healthPoints >= 90) {
-    println(name + " has a few scratches.")
-     } else if (healthPoints >= 75) {
-    println(name + " has some minor wounds.")
+     println(name + " has a few scratches.")
+     } else if (healthPoints >= 75) { // track whether a player is blessed
+       if (isBlessed) {
+       println(name + " has some minor wounds but is healing quite quickly!")
+     } else
+       println(name + " has some minor wounds.")
      } else if (healthPoints >= 15) {
         println(name + " looks pretty hurt.")
-     }  else {
+     } else {
         println(name + " is in awful condition!")
     }
 }
