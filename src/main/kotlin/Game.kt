96436b32a7f7a980f83867f4363610c73acf5c93
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-16 14:58:11 +0300

    Refactoring healthStatus with ranges

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -5,27 +5,28 @@ fun main() {
     val isBlessed : Boolean = true
     val isImmortal = false
 
-    //Aura
-    //Improving aura code with a conditional expression
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
      println(auraColor)
 
     val healthStatus: String = if (healthPoints == 100){
         ("is in excellent condition!")
-    } else if (healthPoints >= 90) {
+        // Refactoring healthStatus with ranges (in 1..5)
+    } else if (healthPoints in 90..99) {
         ("has a few scratches.")
-    } else if (healthPoints >= 75) {
+        //Another type of range (in 5 downTo 1 )
+    } else if (healthPoints in 89 downTo 75) {
         if (isBlessed) {
             ("has some minor wounds but is healing quite quickly!")
         } else {
             ("has some minor wounds.") }
-    } else if (healthPoints >= 15) {
+        //Another type of range (in 5 until 1)
+    } else if (healthPoints in 74 until 15) {
         ("looks pretty hurt.")
     } else {
         ("is in awful condition!")
     }
-    //Player status
+
     println(name + " " + healthStatus)
 }
 
