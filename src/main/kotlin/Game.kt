e2e7544f01bccea50ea1a6a1e1d137dee1ccd467
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-16 09:09:28 +0300

    Initial commit Basic and first implementation.

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val name = "Madrigal"
+    var healthPoints = 100
+
+    if (healthPoints == 100){ // true or false & '==' structure equality operator read "is equal to"
+        println(name + "is in excellent condition!") // if true do this
+    } else {
+        // '+' to append a value to a string is called string concatenation.
+        println(name + "is in awful condition!") // if false do this
+    }
+}
